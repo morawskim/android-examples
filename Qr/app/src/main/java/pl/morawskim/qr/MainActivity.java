0d77f2ce7ca1a8c591d7d248b759e4a2d409ace7
Marcin Morawski <marcin@morawskim.pl>
2017-07-13 19:56:20 +0200

    Add qr demo app
    This app invoke activity from another anroid app - la.droid.qr
    The scan result is pass back

---
+++ Qr/app/src/main/java/pl/morawskim/qr/MainActivity.java
@@ -0,0 +1,35 @@
+package pl.morawskim.qr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "mmo_qr";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startScan(View view) {
+        Intent intent = new Intent("la.droid.qr.scan");
+        intent.putExtra("la.droid.qr.complete", true);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 0);
+        } else {
+            Log.e(TAG, "Activity la.droid.qr.scan not found");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String result = data.getExtras().getString("la.droid.qr.result");
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText(result);
+    }
+}
