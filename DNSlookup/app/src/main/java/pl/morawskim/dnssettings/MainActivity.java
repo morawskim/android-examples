a64782a3c78bc0b26fbc1f9193ce8e3d31c99bcd
Marcin Morawski <marcin@morawskim.pl>
2017-07-22 12:25:59 +0200

    resolve when press enter or click "resolve" button at virtual keyboard

--- DNSlookup/app/src/main/java/pl/morawskim/dnssettings/MainActivity.java
+++ DNSlookup/app/src/main/java/pl/morawskim/dnssettings/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -21,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
         // Otherwise, we get android.os.NetworkOnMainThreadException
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
+        EditText editText = (EditText) findViewById(R.id.hostname);
+        editText.setImeActionLabel(getString(R.string.resolve), KeyEvent.KEYCODE_ENTER);
+        editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                Button button = (Button) findViewById(R.id.resolve_btn);
+
+                if (keyEvent != null && keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    button.performClick();
+                    return true;
+                }
+                if (i == EditorInfo.IME_ACTION_DONE) {
+                    button.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void resolve(View view) {
